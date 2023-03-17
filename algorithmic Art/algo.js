cdc8fc7180d69fbcbab055d6f04fbfe800c78fba
abdalkaderduraalomari <ab_du@uni-bremen.de>
2023-03-17 06:18:33 +0100

    final version

--- algorithmic Art/algo.js
+++ algorithmic Art/algo.js
@@ -1,9 +1,10 @@
-// import {TWO_PI} from "../p5";
-let qop =1
-let ff = 58
+let animation = 0.5
+let ColorFactor = 58
+
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    // creating the canavs with width , height
+    createCanvas(1200, 800, WEBGL);
 
 
 }
@@ -12,42 +13,41 @@ function setup() {
 function draw() {
 
     background(0)
-    let numberOfCircles = 10
+    let numberOfCircles = 7
 
 
-    translate(width / numberOfCircles, height / numberOfCircles)
-    for (let i = 0; i <= numberOfCircles; i++) {
+    for (let i = 1; i <= numberOfCircles; i++) {
 
-        let noiseMax = i/numberOfCircles+1
-        // drawLine(50,90,noiseMax)
-        fill(200 * ff, 0, ff * 2.5, i+25 )
-        drawShape(5/noiseMax, numberOfCircles, qop)
+        let noiseMax = 1/i
+        fill(200 * ColorFactor, 0, ColorFactor * 2.5, i + 25)
+        scale(0.7)
+
+        drawShape(noiseMax, numberOfCircles, animation)
 
     }
 
-    ff+=0.1
+    ColorFactor += 0.8
 
 
-    qop+=0.008
+    animation += 0.008
 
-    // drawShape(noiseMax, phase, numX, numY)
 
 }
 
 function drawShape(noiseMax, numOfCircles, qop) {
-    translate(width / numOfCircles, height / numOfCircles)
     stroke(255)
     strokeWeight(3)
 
 
-    // noFill()
     beginShape()
+
+
     for (let a = 0; a <= TWO_PI + 1; a += 0.08) {
         let xOff = map(sin(a), -1, 1, 0, noiseMax)
         let yOff = map(cos(a), -1, 1, 0, noiseMax)
         let r = map(noise(xOff, yOff), 0, 1, 0, 450)
-        let x = ((r * sin(a)))-width/qop
-        let y = ((r * cos(a*qop)) - height /a )
+        let x = ((r * sin(a))) - width / qop / numOfCircles
+        let y = ((r * cos(a)) - height / numOfCircles)
         vertex(x, y)
 
 
